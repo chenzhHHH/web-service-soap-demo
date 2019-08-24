9cfce43ee97822e5a4ff816948fe1b230300ddf7
chenzh <502804924@qq.com>
2019-08-24 17:36:30 +0800

    finish first demo

---
+++ spring-boot-soap-service/src/main/java/com/example/testws/springbootsoapservice/CountryEndpoint.java
@@ -0,0 +1,25 @@
+package com.example.testws.springbootsoapservice;
+
+import com.example.testws.springbootsoapservice.dto.GetCountryRequest;
+import com.example.testws.springbootsoapservice.dto.GetCountryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URI = "http://127.0.0.1:8080/ws";
+    @Autowired
+    private CountryRepository countryRepository;
+    public CountryEndpoint(CountryRepository countryRepository) { this.countryRepository = countryRepository; }
+    //配置对外接口
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+        return response;
+    }
+}
